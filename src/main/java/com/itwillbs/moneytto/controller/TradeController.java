092ce5ff3bc1a13a85b6ebe6f7062b0e85e1b903
cw <user@DESKTOP-2PGRMF4>
2023-05-17 19:14:13 +0900

    0517상세
    컨트롤러 업데이트

--- src/main/java/com/itwillbs/moneytto/controller/TradeController.java
+++ src/main/java/com/itwillbs/moneytto/controller/TradeController.java
@@ -2,6 +2,8 @@ package com.itwillbs.moneytto.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.itwillbs.moneytto.service.TradeService;
 
@@ -10,4 +12,10 @@ public class TradeController {
 	
 	@Autowired
 	private TradeService tradeService;
+	
+	@RequestMapping(value = "/trade_payment", method = {RequestMethod.GET, RequestMethod.POST})
+	public String tradePayment() {
+		
+		return "trade/trade_payment";
+	}
 }
