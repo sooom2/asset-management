36d590de38d2ed5a0199f047db6b2221b79b2b7f
yerik-12 <129239212+yerik-12@users.noreply.github.com>
2023-05-22 00:23:08 +0900

    marker - merge

--- src/main/java/com/itwillbs/moneytto/service/MarketService.java
+++ src/main/java/com/itwillbs/moneytto/service/MarketService.java
@@ -1,5 +1,7 @@
 package com.itwillbs.moneytto.service;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,4 +12,9 @@ public class MarketService {
 
 	@Autowired
 	private MarketMapper mapper;
+
+	public int insertItem(HashMap<String, String> item) {
+		
+		return mapper.insertItem(item);
+	}
 }
