30c7af4d60bb751f46daba19fac8671a0d644ebd
yerik-12 <129239212+yerik-12@users.noreply.github.com>
2023-05-22 00:23:08 +0900

    item insert

--- src/main/java/com/itwillbs/moneytto/controller/MarketController.java
+++ src/main/java/com/itwillbs/moneytto/controller/MarketController.java
@@ -1,19 +1,23 @@
 package com.itwillbs.moneytto.controller;
 
 import java.util.Date;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.itwillbs.moneytto.service.MarketService;
+
 
 @Controller
 public class MarketController {
 	
 	@Autowired
-	private MarketService marketService;
+	private MarketService service;
 	
 	@GetMapping(value = "market_list")
 	public String marketList() {
@@ -52,4 +56,27 @@ public class MarketController {
 		model.addAttribute("now",now);
 		return "market/market_chat";
 	}
+	
+	
+	@PostMapping(value="itemRegistPro")
+	public String itemRegistPro(@RequestParam HashMap<String, String> item, Model model) {
+		int insertCount = service.insertItem(item);
+		
+		if(insertCount > 0) {
+			
+			model.addAttribute("msg", "상품이 등록되었습니다.");
+			model.addAttribute("target", "main");
+			
+			return "success";
+		} else {
+			model.addAttribute("msg", "상품 등록에 실패하였습니다.");
+			
+			return "fail_back";
+		}
+		
+	}
+	
+	
+	
+	
 }
