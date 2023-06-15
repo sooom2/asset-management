8415c6171e2db8e631f7843cbb5c11fb7fadb9a5
seouna <seouna@YUNA>
2023-06-15 10:26:23 +0900

    수정

--- src/main/java/com/itwillbs/moneytto/socket/MarketChatSocketHandler.java
+++ src/main/java/com/itwillbs/moneytto/socket/MarketChatSocketHandler.java
@@ -55,31 +55,7 @@ public class MarketChatSocketHandler extends TextWebSocketHandler {
 		// 채팅 세션 목록에 채팅방이 존재 X
 		
 		HashMap<String, String> sellDetail = marketChatService.getSellID(item_code);
-		String buyId = target;
-		String sellId = sellDetail.get("member_id");
 												//상대방아이디
-		// 내아이디랑 sellId랑 다르면 buy아이디는 target
-		if(name == sellId) {
-			buyId = target;
-			sellId = name;
-			System.out.println("내가 살때 buy_ID는 target ==============================");
-			System.out.println("사는사람 : " + buyId + "파는사람 : " + sellId);
-			System.out.println("==============================");
-		} else if(name != sellId) {
-			buyId = name;
-			sellId = target;
-			System.out.println("내가 팔때 buyID 는 나 =============================");
-			System.out.println("사는사람 : " + buyId + "파는사람 : " + sellId);
-			System.out.println("==============================");
-		}
-		
-		// name 세션 // sellId 아이템 조회
-		// sellId  = name >>> sellId
-		// target = buyId
-		System.out.println("sellId ===========================");
-		System.out.println(sellId);
-		System.out.println("===========================");
-		
 		if(marketList.get(room_code) == null && messages.contains(name)) {
 			System.out.println("===========================");
 			System.out.println("채팅방 생성됬니");
@@ -106,19 +82,24 @@ public class MarketChatSocketHandler extends TextWebSocketHandler {
 			System.out.println("===========================");
 			System.out.println("생성된 "+room_code+"번 채팅방으로 입장");
 			System.out.println("===========================");
-	            	
-     
 		}
 
 		// 채팅 메세지 입력 시
 		else if(marketList.get(room_code) != null && !messages.contains(name)) {
 			// 채팅 출력
 			
+			String buyId = target;
+			String sellId = sellDetail.get("member_id");
+			
+			if(sellId.equals(buyId)) {
+				buyId = name;
+			}
+			
 			System.out.println("===========================");
 			System.out.println(room_code);
 			System.out.println("===========================");
 			TextMessage textMessage = new TextMessage(name + ":" + messages);
-                
+              
 			int sessionCount = 0;
 //			session.sendMessage(textMessage);
 			
